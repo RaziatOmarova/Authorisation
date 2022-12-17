8c92fa5d6120485251191bc086ac5e222ae8ea20
Raziat <114012022+RaziatOmarova@users.noreply.github.com>
2022-12-17 22:59:09 +0300

    Добавьте файлы проекта.

---
+++ Authorisation/Views/Pages/FirstPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Authorisation.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для FirstPage.xaml
+    /// </summary>
+    public partial class FirstPage : Page
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+        private void BtnNext_Click(object sender, RoutedEventArgs e)
+        {
+            string Login = "Login";
+            string Password = "Password";
+            if (TxtBxLogin.Text == Login && TxtBxPassword.Text == Password)
+            {
+                NavigationService.Navigate(new SecondPage());
+            }
+            else
+            {
+                MessageBox.Show("Incorret data!");
+            }
+            
+        }
+    }
+}
